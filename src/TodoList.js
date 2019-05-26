ee3dcb8d238d328b0a4422384c3e11e1b7c9041c
shen wang <swan2892@uni.sydney.edu.au>
2019-05-26 16:46:15 +1000

    update

--- src/TodoList.js
+++ src/TodoList.js
@@ -7,13 +7,22 @@ class TodoList extends Component {
     constructor(props) {
         super(props);
         this.state = store.getState();
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleStoreChange = this.handleStoreChange.bind(this);
+        this.handleButtonClick = this.handleButtonClick.bind(this);
+        store.subscribe(this.handleStoreChange);
     }
     render() {
         return (
             <div style={{marginTop: '10px', marginLeft: '10px'}}>
                 <div>
-                    <Input value={this.state.inputValue} placeholder='todolist' style={{width: 300, marginRight: '10px'}}/>
-                    <Button type="primary">submit</Button>
+                    <Input 
+                        value={this.state.inputValue} 
+                        placeholder='todolist' 
+                        style={{width: 300, marginRight: '10px'}}
+                        onChange={this.handleInputChange}
+                    />
+                    <Button onClick={this.handleButtonClick} type="primary">submit</Button>
                 </div>
                 <List
                 style={{marginTop: '10px', width: '300px'}}
@@ -30,6 +39,24 @@ class TodoList extends Component {
             </div>
         )
     }
-}
 
+
+    handleInputChange(e) {
+        const action = {
+            type: 'change_input_value',
+            value: e.target.value
+        }
+        store.dispatch(action);
+    }
+
+    handleStoreChange() {
+        this.setState(store.getState());
+    }
+
+    handleButtonClick() {
+        const action = {
+            type: 'add_todo_item'
+        }
+        store.dispatch(action);    }
+}
 export default TodoList;
