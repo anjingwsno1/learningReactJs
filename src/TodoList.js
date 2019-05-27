e3c72dda69b6baef13d9c8af4ce577e83d20a27c
shen wang <swan2892@uni.sydney.edu.au>
2019-05-27 18:13:07 +1000

    update

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,7 +1,8 @@
 import React, { Component} from 'react';
 import 'antd/dist/antd.css';
-import { Input, Button, List } from 'antd';
 import store from './store';
+import { getInputChangeAction, getAddItemAction, getDeleteItemAction } from './store/actionCreators';
+import TodoListUI from './TodoListUI';
 
 class TodoList extends Component {
     constructor(props) {
@@ -10,42 +11,23 @@ class TodoList extends Component {
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleStoreChange = this.handleStoreChange.bind(this);
         this.handleButtonClick = this.handleButtonClick.bind(this);
+        this.handleItemDelete = this.handleItemDelete.bind(this);
         store.subscribe(this.handleStoreChange);
     }
     render() {
-        return (
-            <div style={{marginTop: '10px', marginLeft: '10px'}}>
-                <div>
-                    <Input 
-                        value={this.state.inputValue} 
-                        placeholder='todolist' 
-                        style={{width: 300, marginRight: '10px'}}
-                        onChange={this.handleInputChange}
-                    />
-                    <Button onClick={this.handleButtonClick} type="primary">submit</Button>
-                </div>
-                <List
-                style={{marginTop: '10px', width: '300px'}}
-                header={<div>Header</div>}
-                footer={<div>Footer</div>}
-                bordered
-                dataSource={this.state.list}
-                renderItem={item => (
-                    <List.Item>
-                    {item}
-                    </List.Item>
-                )}
-                />
-            </div>
-        )
+        return <TodoListUI 
+                inputValue={this.state.inputValue}
+                list={this.state.list}
+                handleInputChange={this.handleInputChange}
+                handleButtonClick={this.handleButtonClick}
+                handleItemDelete={this.handleItemDelete}
+            />
     }
 
 
     handleInputChange(e) {
-        const action = {
-            type: 'change_input_value',
-            value: e.target.value
-        }
+
+        const action = getInputChangeAction(e.target.value);
         store.dispatch(action);
     }
 
@@ -54,9 +36,12 @@ class TodoList extends Component {
     }
 
     handleButtonClick() {
-        const action = {
-            type: 'add_todo_item'
-        }
-        store.dispatch(action);    }
+        const action = getAddItemAction();
+        store.dispatch(action);    
+    }
+    handleItemDelete(index) {
+        const action = getDeleteItemAction(index);
+        store.dispatch(action);
+    }
 }
 export default TodoList;
---
+++ src/store/actionCreators.js
@@ -0,0 +1,14 @@
+import { CAHNGE_INPUT_VALUE, ADD_TODO_ITEM, DELETE_TODO_ITEM } from "./actionTypes";
+
+export const getInputChangeAction = (value) =>({
+    type: CAHNGE_INPUT_VALUE,
+    value
+});
+
+export const getAddItemAction = () =>({
+    type: ADD_TODO_ITEM
+});
+
+export const getDeleteItemAction = () =>({
+    type: DELETE_TODO_ITEM
+});
